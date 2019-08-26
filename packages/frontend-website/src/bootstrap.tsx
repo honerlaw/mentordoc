329e7016a92d5bcbdc55a19ca2b42f8f604082ff
Derek <derek@Ceross-MBP.lan>
2019-08-25 21:59:19 -0400

    fix import

--- packages/frontend-website/src/bootstrap.tsx
+++ packages/frontend-website/src/bootstrap.tsx
@@ -2,8 +2,8 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import {Main} from "./component/main";
 import {Provider} from "react-redux";
-import {RootStore} from "@honerlawd/mentordoc-frontend-shared/dist/store/store";
 import {BrowserRouter} from "react-router-dom";
+import {RootStore} from "@honerlawd/mentordoc-frontend-shared/dist/store/store";
 
 const container: HTMLElement = document.createElement("div");
 container.id = "mentordoc-mount";
