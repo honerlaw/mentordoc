061ef932b4067557f152e3495332a0fb64802fc8
Derek <derek@Ceross-MBP.lan>
2019-09-02 16:08:12 -0400

    only render when path is to actual document

--- packages/frontend-website/src/component/app/dashboard-viewer.tsx
+++ packages/frontend-website/src/component/app/dashboard-viewer.tsx
@@ -1,13 +1,32 @@
 import * as React from "react";
 import "./dashboard-viewer.scss";
 import {DocumentViewer} from "./dashboard-viewer/document-viewer";
+import {WithRouter} from "@honerlawd/mentordoc-frontend-shared/dist/store/decorator/with-router";
+import {RouteComponentProps} from "react-router";
 
-export class DashboardViewer extends React.PureComponent<{}, {}> {
+interface IRouteProps {
+    orgId: string;
+    docId: string;
+}
+
+interface IProps extends Partial<RouteComponentProps<IRouteProps>> {
+
+}
+
+@WithRouter()
+export class DashboardViewer extends React.PureComponent<IProps, {}> {
 
     public render(): JSX.Element {
         return <div className={"dashboard-viewer"}>
-            <DocumentViewer />
+            {this.renderViewer()}
         </div>
+    }
+
+    private renderViewer(): JSX.Element | null {
+        if (this.props.match!.params.docId && this.props.match!.params.orgId) {
+            return <DocumentViewer/>;
+        }
+        return null;
     }
 
 }
