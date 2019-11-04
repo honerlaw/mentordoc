c214ebb78dfd3d3dc87df95055f84463ab68e944
Derek <derek@Ceross-MBP-2.lan>
2019-11-03 21:33:51 -0500

    styled search results

--- packages/frontend-website/src/component/shared/header/search.tsx
+++ packages/frontend-website/src/component/shared/header/search.tsx
@@ -12,6 +12,11 @@ import {
     ISetSearchDocumentsSelector, SetSearchDocuments
 } from "@honerlawd/mentordoc-frontend-shared/dist/store/action/document/set-search-documents";
 import {AclDocument} from "@honerlawd/mentordoc-frontend-shared/dist/store/model/document/acl-document";
+import {Link} from "react-router-dom";
+import {DocumentDraft} from "@honerlawd/mentordoc-frontend-shared/dist/store/model/document/document-draft";
+import {Document} from "@honerlawd/mentordoc-frontend-shared/dist/store/model/document/document";
+import "./search.scss";
+import * as moment from "moment";
 
 interface IProps extends Partial<IDispatchPropMap<ISearchDocumentsDispatch> & ISelectorPropMap<ISetSearchDocumentsSelector>> {
 
@@ -38,22 +43,31 @@ export class Search extends React.PureComponent<IProps, IState> {
 
     public render(): JSX.Element {
         return <div className={"search"}>
-            <input type={"text"} placeholder={"search"} onChange={this.onChange} />
-            <div className={"search-results"}>
-                {this.renderSearchResults()}
-            </div>
+            <input type={"text"} placeholder={"search"} onChange={this.onChange}/>
+            {this.renderSearchResults()}
         </div>;
     }
 
-    private renderSearchResults(): JSX.Element[] {
+    private renderSearchResults(): JSX.Element | null {
         const docs: AclDocument[] | null = this.props.selector!.searchDocuments;
         if (!docs) {
-            return [];
+            return null;
         }
 
-        return docs.map((doc: AclDocument): JSX.Element => {
-            return <span>{doc.model.drafts[0].name}</span>
-        });
+        return <ul className={"search-results"}>
+            {docs.map((aclDoc: AclDocument): JSX.Element => {
+                const doc: Document = aclDoc.model;
+                const draft: DocumentDraft = aclDoc.model.drafts[0];
+                const timeago: string = moment((draft.publishedAt || draft.updatedAt) / 1e+6).fromNow();
+
+                return <li>
+                    <Link to={`/app/${doc.organizationId}/${doc.id}`}>
+                        <div className={"title"}>{draft.name}</div>
+                        <div className={"timeago"}>last updated {timeago}</div>
+                    </Link>
+                </li>
+            })}
+        </ul>;
     }
 
     private async onChange(event: React.ChangeEvent<HTMLInputElement>): Promise<void> {
